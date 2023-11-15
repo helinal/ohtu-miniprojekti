776499761bf3a95f0ba574475b012c3dfaaac663
Kalle Hahl <kalle.hahl@hotmail.com>
2023-11-15 13:26:35 +0200

    modified bibtex.py

--- src/services/bibtex.py
+++ src/services/bibtex.py
@@ -2,24 +2,21 @@ class Bibtex():
     def __init__(self, docutype, citekey):
         self.docutype = docutype
         self.citekey = citekey
-        self.bibDict = {}
-        
+        self.bibDict = {
+}
     def __str__(self):
         looped = self.loop_to_string()
         return(
-            f"@{self.docutype}\{self.citekey},"
+            f"@{self.docutype}{{{self.citekey},\n"
             f"{looped}"
-            f"\"
-            )
-        
-    def loop_to_string():
+            f"}}"
+        )
+    def loop_to_string(self):
         ret = ""
-        for x in self.bibDict:
-            ret += "\n  "
-            ret += x
-            ret += " = \""
-            ret += bibDict(x)
-            ret += "\","
+        for key, value in self.bibDict.items():
+            ret += f"    {key} = {value}\n"
+        return ret
 
-    def add(self,key, value):
+    def add(self, key, value):
         self.bibDict[key] = value
+

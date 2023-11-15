bc48bc59ebe56a74a8af5faf16090ebaeb584f02
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-11-15 13:07:36 +0200

    first version of IO

--- src/services/bibtex.py
+++ src/services/bibtex.py
@@ -3,25 +3,16 @@ class Bibtex():
         self.docutype = docutype
         self.citekey = citekey
         self.bibDict = {}
-<<<<<<< HEAD
-=======
         
->>>>>>> 51dea41 ( first version of IO)
     def __str__(self):
         looped = self.loop_to_string()
         return(
             f"@{self.docutype}\{self.citekey},"
             f"{looped}"
-<<<<<<< HEAD
-            f"\}"
-        )
-    def loop_to_string(self):
-=======
             f"\"
             )
         
     def loop_to_string():
->>>>>>> 51dea41 ( first version of IO)
         ret = ""
         for x in self.bibDict:
             ret += "\n  "
@@ -30,9 +21,5 @@ class Bibtex():
             ret += bibDict(x)
             ret += "\","
 
-<<<<<<< HEAD
-    def add(self, key, value):
-=======
     def add(self,key, value):
->>>>>>> 51dea41 ( first version of IO)
         self.bibDict[key] = value

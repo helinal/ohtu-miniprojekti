e70f0dd09db1240f5223547df54a0ffca4f41428
lassisav <lassi.savolainen@helsinki.fi>
2023-11-15 12:52:46 +0200

    Added bibtex-class.

---
+++ src/services/bibtex.py
@@ -0,0 +1,23 @@
+class Bibtex():
+    def __init__(self, docutype, citekey):
+        self.docutype = docutype
+        self.citekey = citekey
+        self.bibDict = {}
+    def __str__():
+        looped = self.loop_to_string()
+        return(
+            f"@{docutype}\{{citekey},"
+            f"{looped}"
+            f"\}"
+        )
+    def loop_to_string():
+        ret = ""
+        for x in self.bibDict:
+            ret += "\n  "
+            ret += x
+            ret += " = \""
+            ret += bibDict(x)
+            ret += "\","
+
+    def add(key, value):
+        self.bibDict[key] = value

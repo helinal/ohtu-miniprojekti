c603b636629fb35734f45b938cb4906c8f9fbdf6
pakkanep <patrikpakkanen9@gmail.com>
2023-11-20 12:30:30 +0200

    Initialized test file for class UI

--- src/services/tests/IO_test.py
+++ src/services/tests/IO_test.py
@@ -1,10 +1,8 @@
 import unittest
 from unittest.mock import patch
-from IO import KonsoliIO
 from IO import UI
 
 
-class TestKonsoliIO(unittest.TestCase):
+class TestUI(unittest.TestCase):
     def setUp(self):
-        self.konsoli_io = KonsoliIO()
-        self.ui = UI()
+        pass

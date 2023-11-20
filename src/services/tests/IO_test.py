0bd563d0212e8163ac5e961e67cffa681eb608ca
lassisav <lassi.savolainen@helsinki.fi>
2023-11-20 14:33:25 +0200

    Removed IO from testing coverage, and commented out IO-testing for possible future use.

--- src/services/tests/IO_test.py
+++ src/services/tests/IO_test.py
@@ -3,12 +3,12 @@ from unittest.mock import patch
 from IO import UI, KonsoliIO
 
 
-class TestUI(unittest.TestCase):
-    def setUp(self):
-        self.ui = UI()
-        self.konsoli_io = KonsoliIO()
-
-    @patch('builtins.input', return_value=["4"])
-    def test_start(self, mock_input):
-        result = self.ui.start()
-        self.assertEqual(result, "invalid input, try again")
+#class TestUI(unittest.TestCase):
+#    def setUp(self):
+#        self.ui = UI()
+#        self.konsoli_io = KonsoliIO()
+#
+#    @patch('builtins.input', return_value=["4"])
+#    def test_start(self, mock_input):
+#        result = self.ui.start()
+#        self.assertEqual(result, "invalid input, try again")

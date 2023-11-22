adb1ffe5fce4e6699ae830ac158b8e3f34d5c167
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-11-22 22:42:20 +0200

    IO.py eka testi

--- src/services/tests/IO_test.py
+++ src/services/tests/IO_test.py
@@ -1,14 +1,29 @@
 import unittest
-from unittest.mock import patch
-from IO import UI, KonsoliIO
+from IO import UI
+from app_logic import AppLogic
+
+class StubIO:
+    def __init__(self, inputs: list):
+        self.inputs = inputs
+        self.outputs = []
+
+    def read_input(self, text):
+        return self.inputs.pop(0)
+
+    def write_screen(self, text):
+        self.outputs.append(text)
 
 
-#class TestUI(unittest.TestCase):
-#    def setUp(self):
-#        self.ui = UI()
-#        self.konsoli_io = KonsoliIO()
-#
-#    @patch('builtins.input', return_value=["4"])
-#    def test_start(self, mock_input):
-#        result = self.ui.start()
-#        self.assertEqual(result, "invalid input, try again")
+class TestUI(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tester(self):
+        self.stub_io = StubIO(["4", "3"])
+        self.ui = UI(self.stub_io)
+        self.ui.start()
+        
+
+        expected_output = ["invalid input, try again"]
+
+        self.assertEqual(self.stub_io.outputs, expected_output)

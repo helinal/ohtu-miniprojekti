4b721d0205b042f782b8b13f7f8804749d2bef3b
Kalle Hahl <kalle.hahl@hotmail.com>
2023-11-24 16:20:29 +0200

    added task for initializing database

--- tasks.py
+++ tasks.py
@@ -27,3 +27,7 @@ def lint(ctx):
 @task
 def format(ctx):
     ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+@task
+def build(ctx):
+    ctx.run("python3 src/initialize_database.py", pty=True)

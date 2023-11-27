f3dd63f74198c076fbd832b25bde975badd34bc6
pakkanep <patrikpakkanen9@gmail.com>
2023-11-27 12:39:30 +0200

    Updated AppLogic tests

--- src/tests/app_logic_test.py
+++ src/tests/app_logic_test.py
@@ -1,22 +1,28 @@
 import unittest
+from unittest.mock import Mock, ANY, patch
 from services.app_logic import AppLogic
 from services.bibtex import Bibtex
 
 
+
 class TestAppLogic(unittest.TestCase):
     def setUp(self):
-        self.app_logic = AppLogic()
+        self.bib_repo = Mock()
+        self.app_logic = AppLogic(self.bib_repo)
         self.obj = Bibtex("entrytype", "cite")
         self.comparison_list = [self.obj]
 
-    def test_constructor(self):
-        self.assertEqual(self.app_logic.citations, [])
-        self.assertEqual(self.comparison_list, [self.obj])
+    def test_initialize_citations(self):
+        value = self.app_logic.initialize_citations()
+        self.assertEqual(value, self.bib_repo.fetch_all())
 
-    def test_add(self):
+    def test_return_all_citations(self):
+        value = self.app_logic.return_all()
+        comparison_value = self.bib_repo.fetch_all()
+        self.assertEqual(value, comparison_value)
+    
+    def test_add_citation(self):
         self.app_logic.add(self.obj)
-        self.assertEqual(self.comparison_list, self.app_logic.citations)
-
-    def test_return_all(self):
-        self.app_logic.add(self.obj)
-        self.assertEqual(self.comparison_list, self.app_logic.return_all())
+        value = self.app_logic.return_all()
+        comparison_value = self.bib_repo.fetch_all()
+        self.assertEqual(value, comparison_value)

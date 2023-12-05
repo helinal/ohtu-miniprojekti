1c40eb2e8d7bf9b53a6affbb3195d826fc1f6b7c
Emil Malk <emil.malk@gmail.com>
2023-12-05 18:28:42 +0200

    Made variable names more representative.

--- src/services/IO.py
+++ src/services/IO.py
@@ -5,14 +5,14 @@ class KonsoliIO():
     def write_screen(self, text):
         print(text)
 
-    def print_readable_form(self, text):
-        if not text:
+    def print_readable_form(self, references):
+        if not references:
             print("\nYou do not have any references to print!\n")
 
-        for i in text:
-            doc_type = i.docutype
-            citekey = i.citekey
+        for ref in references:
+            doc_type = ref.docutype
+            citekey = ref.citekey
 
             print(f"\nType: {doc_type}, Citekey: {citekey}\n")
-            for key, value in i.bibDict.items():
+            for key, value in ref.bibDict.items():
                 print(f"{key:10} {value}")
